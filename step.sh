f12bb7797da546b629bec2b42fec02445770731e
Aaron Sapp <sapp.aj@pg.com>
2021-03-30 12:22:11 -0400

    cleaned up script

--- step.sh
+++ step.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -ex
 
-echo "path_to_binary: ${path_to_binary}, platform: ${platform}, api_key: ${api_key}"
+RESULT=$(curl -F app=@${path_to_binary} -F key=${api_key} -F platform="${platform}" https://emm.kryptowire.com/api/submit)
 
-IP=$(curl -F app=@${path_to_binary} -F key=${api_key} -F platform="${platform}" https://emm.kryptowire.com/api/submit)
-
-if [[ $IP = *"invalid_auth"* ]]; then
+if [[ $RESULT = *"invalid_auth"* ]]; then
   exit 1
 fi
 
-envman add --key KRYPTOWIRE_UUID --value IP
+envman add --key KRYPTOWIRE_UUID --value $RESULT["uuid"]
